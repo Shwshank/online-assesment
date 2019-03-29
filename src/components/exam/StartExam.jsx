9943f848184467842b5c8abee53cccb5631418f6
Durgesh Singh <singh.durgesh5@gmail.com>
2019-03-29 09:59:29 +0530

    ui changes

--- src/components/exam/StartExam.jsx
+++ src/components/exam/StartExam.jsx
@@ -2,20 +2,22 @@ import React from "react";
 import { connect } from "react-redux";
 
 class StartExam extends React.Component {
-
   render() {
-    return(
-      <div>
-      {this.props.examUser.name}
-      </div>
-    )
+    return (
+      <main style={{ minHeight: 500 }}>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12" />
+            {this.props.examUser.name}
+          </div>
+        </div>
+      </main>
+    );
   }
 }
 
-const mapStateToProps = (state) =>{
-return { examUser: state.examUser }
-}
+const mapStateToProps = state => {
+  return { examUser: state.examUser };
+};
 
-export default connect(
-mapStateToProps
-)(StartExam);
+export default connect(mapStateToProps)(StartExam);
